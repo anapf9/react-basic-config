e19c25d5884c49281677fe7b8f457007fa355ed5
Ana <ana.eng.dev@gmail.com>
2020-06-11 10:55:47 -0300

    estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header'
 
 function App() {
+  const [projects, setProjects] = useState([
+    'dev web',
+    'frontend'
+  ]) 
+  // use state retorna um array com 2 posições
+  //1.  Varivael com o valor inicial
+  //2. Função para atualizar o valor
+  function handleAddProject() {
+    // projects.push(`Novo projeto ${Date.now()}`) // alterando o array original
+
+    // dessa forma é como se criasse um novo Array a partir do original e acrescenta o valor desejado nesse novo array sem mudar o original
+    // então setamos para usar o novo array
+    setProjects([...projects, `Novo projeto ${Date.now()}`]) // aplicando o conceito de imutabilidade
+    
+    // console.log(projects)
+  }
   return (
     <>
     <Header title="homepage"> 
-    <ul>
-      <li> qualquer coisa</li>
-      <li> qualquer coisa 2</li>
-    </ul>
     </Header>
-    <Header title="Teste props">
     <ul>
-      <li> Outra</li>
-      <li> teste coisa 2</li>
+      {projects.map(project => <li key={project}>{project}</li> )}
     </ul>
-    </Header>
+
+    <button type="button" onClick={handleAddProject}>Add</button>
     </>
   )
 }
