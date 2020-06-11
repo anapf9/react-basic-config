84d298288e979e365cd8385c8cc8d998913a9b8d
Ana <ana.eng.dev@gmail.com>
2020-06-11 10:07:47 -0300

    fix babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
-const path = require('path')
+const path = require("path"); //biblioteca global
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, "src", "index.js"), //dirname retorna o diretorio atual do arquivo. Cada virgula depois dele é como uma barra
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    // s seguir o código transpilado pelo babel
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js" //nome do arquivo que será gerado
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
+    //monitora para atualizar quando for necessário
+    contentBase: path.resolve(__dirname, "public") //pasta ond fica o bundle.js
   },
   module: {
     rules: [
-      // cada objeto é um loader 
       {
-        test: /\.js$/,
+        //transpiladores para código não javacript
+        test: /\.js$/, //todos os arquivos .js vão para essa regra
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       }
     ]
   }
-}
+};
