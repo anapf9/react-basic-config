92b05339bce11b8934bbf7178d33ca4841262388
Ana <ana.eng.dev@gmail.com>
2020-06-11 10:39:28 -0300

    propriedades

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-    <Header />
-    <Header />
+    <Header title="homepage"> 
+    <ul>
+      <li> qualquer coisa</li>
+      <li> qualquer coisa 2</li>
+    </ul>
+    </Header>
+    <Header title="Teste props">
+    <ul>
+      <li> Outra</li>
+      <li> teste coisa 2</li>
+    </ul>
+    </Header>
     </>
   )
 }
